a1bdf76884655ebe77656ac2acd4ce227cbf5bfa
Thomas Koman <99066789@mydavinci.nl>
2022-01-18 12:43:23 +0100

    made a bg changing button program

--- lightswitch.py
+++ lightswitch.py
@@ -1,3 +1,4 @@
+from glob import glob
 import tkinter as tk
 window = tk.Tk()
 
@@ -6,6 +7,21 @@ button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
 
+#Defines the state of the color of the background and the text of the button
+buttonstates = ["off", "on"]
+screenstates = ["black", "yellow"]
+
+def updateState():#Updates the text of the button and the color of the background
+    global buttonstates, screenstates, button
+    buttonstates.reverse()
+    screenstates.reverse()
+    
+    button.config(text=f"Turn the screen {buttonstates[1]}")
+    window.config(bg=screenstates[0])
+
+#Pre-Defines the background and the button
+window.config(bg=screenstates[0])
+button.config(text=f"Turn the screen {buttonstates[1]}",command=updateState)
 
 
 # schijf hier tussen je code
